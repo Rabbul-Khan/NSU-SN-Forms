c22b10f1666d85b83d4e49806af650f42bd32bbe
Rabbul-Khan <mohammed.rabbul.hossain.khan@g.bracu.ac.bd>
2024-01-25 21:22:55 +0600

    Complete form functionality and design

--- components/ui/ContactUs.jsx
+++ components/ui/ContactUs.jsx
@@ -7,11 +7,11 @@ import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "./card";
 import { Button } from "./button";
+import { toast } from "sonner";
 
 export const ContactUs = () => {
   const form = useRef();
@@ -19,36 +19,38 @@ export const ContactUs = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    // emailjs
-    //   .sendForm(
-    //     'service_me9rx0l',
-    //     'template_e0h96qw',
-    //     form.current,
-    //     'aJz1bNkxownFkofBi'
-    //   )
-    //   .then(
-    //     (result) => {
-    //       console.log(result.text);
-    //     },
-    //     (error) => {
-    //       console.log(error.text);
-    //     }
-    //   );
+    emailjs
+      .sendForm(
+        "service_me9rx0l",
+        "template_e0h96qw",
+        form.current,
+        "aJz1bNkxownFkofBi",
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          form.current.reset();
+          toast.success("Your form has been submitted");
+        },
+        (error) => {
+          console.log(error.text);
+        },
+      );
   };
 
   return (
-    <div className="max-w-1/2 flex w-1/2 min-w-full flex-col items-center justify-center rounded-md bg-white p-5 shadow-lg sm:min-w-min xl:flex-row">
+    <div className="max-w-1/2 flex h-full w-1/2 min-w-full flex-col items-center justify-center rounded-md bg-white p-5 shadow-lg sm:h-[90%] sm:min-w-min xl:h-[70%] xl:w-[1000px] xl:flex-row">
       <div className="flex w-auto items-center justify-center xl:w-1/2">
         <img
           src="contact.svg"
           alt=""
-          className="max-w-sm md:max-w-md xl:max-w-full"
+          className="min-w-10 max-w-80 px-10 md:max-w-md xl:max-w-xl xl:px-10"
         />
       </div>
       <Card className=" xl:w-1/2">
         <CardHeader>
-          <CardTitle className="text-3xl">Get in Touch!</CardTitle>
-          <CardDescription>
+          <CardTitle className="text-3xl xl:text-4xl">Get in Touch!</CardTitle>
+          <CardDescription className="xl:text-md">
             Feel free to reach out to us using the form below
           </CardDescription>
         </CardHeader>
@@ -58,27 +60,31 @@ export const ContactUs = () => {
             ref={form}
             onSubmit={sendEmail}
             id="contactForm"
-            className="grid w-full max-w-sm items-center gap-3"
+            className="grid w-full max-w-sm items-center gap-5"
           >
             <div className="grid w-full max-w-sm items-center gap-2">
-              <Label htmlFor="user_name">Name</Label>
+              <Label htmlFor="user_name">
+                Name <sup className="text-xs text-red-500">*</sup>
+              </Label>
               <Input
                 required
                 type="text"
                 id="user_name"
                 placeholder="John Doe"
-                className="shadow-sm focus-visible:ring-[#a6c1ee] invalid:focus-visible:ring-[#e2797e]"
+                className=""
               />
             </div>
 
             <div className="grid w-full max-w-sm items-center gap-2">
-              <Label htmlFor="user_email">Email</Label>
+              <Label htmlFor="user_email">
+                Email <sup className="text-xs text-red-500">*</sup>
+              </Label>
               <Input
                 required
                 type="email"
                 id="user_email"
                 placeholder="john.doe@gmail.com"
-                className="shadow-sm focus-visible:ring-[#a6c1ee] invalid:focus-visible:ring-[#e2797e]"
+                className=""
               />
             </div>
 
@@ -88,29 +94,28 @@ export const ContactUs = () => {
                 type="text"
                 id="subject"
                 placeholder="Provide a concise subject..."
-                className="shadow-sm focus-visible:ring-[#a6c1ee] "
+                className=""
               />
             </div>
 
             <div className="grid w-full max-w-sm items-center gap-2">
-              <Label htmlFor="message">Message</Label>
+              <Label htmlFor="message">
+                Message <sup className="text-xs text-red-500">*</sup>
+              </Label>
               <Textarea
+                required
                 id="message"
                 placeholder="Type your message here..."
-                className="shadow-sm focus-visible:ring-[#a6c1ee] "
+                className=""
               />
             </div>
 
             <Button
               type="submit"
-              className="relative ml-auto mt-2 flex h-[50px] w-40 items-center justify-center overflow-hidden bg-[#fbc2eb] text-black shadow-2xl transition-all before:absolute before:h-0 before:w-0 before:rounded-full before:bg-[#a6c1ee] before:duration-700 before:ease-out hover:scale-105 hover:shadow-[#a6c1ee] hover:before:h-56 hover:before:w-56 active:scale-95"
+              className="relative ml-auto mt-6 flex h-[50px] w-40 items-center justify-center overflow-hidden bg-[#fbc2eb] text-black shadow-2xl transition-all before:absolute before:h-0 before:w-0 before:rounded-full before:bg-[#a6c1ee] before:duration-700 before:ease-out hover:scale-105 hover:shadow-[#a6c1ee] hover:before:h-56 hover:before:w-56 active:scale-95"
             >
               <span className="relative z-10">Submit</span>
             </Button>
-            {/* 
-            <button class="">
-              <span class="relative z-10">Circle hover</span>
-            </button> */}
           </form>
         </CardContent>
       </Card>
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { ContactUs } from "../components/ui/ContactUs";
+import { Toaster } from "@/components/ui/sonner";
 
 function App() {
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-gradient-to-r from-[#fbc2eb] to-[#a6c1ee] font-primary">
       <ContactUs />
+      <Toaster richColors />
     </div>
   );
 }
