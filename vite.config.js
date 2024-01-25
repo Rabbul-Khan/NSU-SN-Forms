e17294be3c0da185964a677f8a01c2ed7e08e4bf
Rabbul-Khan <mohammed.rabbul.hossain.khan@g.bracu.ac.bd>
2024-01-25 21:33:47 +0600

    Update README

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
-import path from 'path';
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-import { fileURLToPath } from 'url';
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
 
 // https://vitejs.dev/config/
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
+  base: "/NSU-SN-Forms/",
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './'),
+      "@": path.resolve(__dirname, "./"),
     },
   },
 });
