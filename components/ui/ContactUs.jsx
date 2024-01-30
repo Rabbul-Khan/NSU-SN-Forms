9da8a829cf068022ed551817dad142465f9ee7ab
Rabbul-Khan <mohammed.rabbul.hossain.khan@g.bracu.ac.bd>
2024-01-30 23:29:47 +0600

    Fix responsiveness bugs

--- components/ui/ContactUs.jsx
+++ components/ui/ContactUs.jsx
@@ -39,82 +39,73 @@ export const ContactUs = () => {
   };
 
   return (
-    <div className="max-w-1/2 flex h-full w-1/2 min-w-full flex-col items-center justify-center rounded-md bg-white p-5 shadow-lg sm:h-[90%] sm:min-w-min xl:w-[1000px] xl:flex-row">
-      <div className="hidden max-h-full w-auto items-center justify-center xl:flex xl:w-1/2">
-        <img
-          src="contact.svg"
-          alt=""
-          className="min-w-10 max-w-80 px-10 md:max-w-md xl:max-w-xl xl:px-10"
-        />
-      </div>
-      <Card className=" xl:w-1/2">
+    <div className="flex flex-col items-center justify-center bg-[#FEF6FC] sm:rounded-xl sm:p-5 sm:shadow-lg lg:flex-row xl:w-[1000px]">
+      <img
+        src="contact.svg"
+        alt="Illustrated image of a woman holding an envelope. A mobile and a phone is in the background."
+        className="max-w-80 sm:pt-5 md:max-w-96 lg:pt-0 xl:max-w-[28rem]"
+      />
+
+      <Card>
         <CardHeader>
-          <CardTitle className="text-3xl xl:text-4xl">Get in Touch!</CardTitle>
-          <CardDescription className="xl:text-md">
+          <CardTitle className="text-3xl lg:text-4xl">Get in Touch!</CardTitle>
+          <CardDescription className="text-sm lg:text-base">
             Feel free to reach out to us using the form below
           </CardDescription>
         </CardHeader>
 
-        <CardContent className="">
+        <CardContent>
           <form
             ref={form}
             onSubmit={sendEmail}
             id="contactForm"
-            className="grid w-full max-w-sm items-center gap-5"
+            className="grid w-full items-center gap-5"
           >
-            <div className="grid w-full max-w-sm items-center gap-2">
-              <Label htmlFor="user_name">
-                Name <sup className="text-xs text-red-500">*</sup>
-              </Label>
+            <div className="grid gap-2">
+              <Label>Name</Label>
               <Input
                 required
                 type="text"
-                id="user_name"
+                name="user_name"
                 placeholder="John Doe"
-                className=""
               />
             </div>
 
-            <div className="grid w-full max-w-sm items-center gap-2">
-              <Label htmlFor="user_email">
-                Email <sup className="text-xs text-red-500">*</sup>
-              </Label>
+            <div className="grid gap-2">
+              <Label>Email</Label>
               <Input
                 required
                 type="email"
-                id="user_email"
+                name="user_email"
                 placeholder="john.doe@gmail.com"
-                className=""
               />
             </div>
 
-            <div className="grid w-full max-w-sm items-center gap-2">
-              <Label htmlFor="subject">Subject</Label>
+            <div className="grid gap-2">
+              <Label>
+                Subject <span className="text-slate-500">(optional)</span>
+              </Label>
               <Input
                 type="text"
-                id="subject"
+                name="subject"
                 placeholder="Provide a concise subject..."
-                className=""
               />
             </div>
 
-            <div className="grid w-full max-w-sm items-center gap-2">
-              <Label htmlFor="message">
-                Message <sup className="text-xs text-red-500">*</sup>
-              </Label>
+            <div className="grid gap-2">
+              <Label>Message</Label>
               <Textarea
                 required
-                id="message"
+                name="message"
                 placeholder="Type your message here..."
-                className=""
               />
             </div>
 
             <Button
               type="submit"
-              className="relative ml-auto mt-6 flex h-[50px] w-40 items-center justify-center overflow-hidden bg-[#fbc2eb] text-black shadow-2xl transition-all before:absolute before:h-0 before:w-0 before:rounded-full before:bg-[#a6c1ee] before:duration-700 before:ease-out hover:scale-105 hover:shadow-[#a6c1ee] hover:before:h-56 hover:before:w-56 active:scale-95"
+              className="relative ml-auto mt-5 h-[50px] w-full overflow-hidden bg-[#fbc2eb] text-base text-[#333333] shadow-2xl transition-all before:absolute before:h-0 before:w-0 before:rounded-full before:bg-[#a6c1ee] before:duration-700 before:ease-out hover:scale-105 hover:shadow-[#a6c1ee] hover:before:h-96 hover:before:w-96 active:scale-95 lg:w-44  lg:hover:before:h-56 lg:hover:before:w-56"
             >
-              <span className="relative z-10">Submit</span>
+              <span className="relative z-10">Send Message</span>
             </Button>
           </form>
         </CardContent>
--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/sonner";
 
 function App() {
   return (
-    <div className="flex h-screen w-screen items-center justify-center bg-gradient-to-r from-[#fbc2eb] to-[#a6c1ee] font-primary">
+    <div className="flex h-screen w-screen items-center justify-center bg-[#FEF6FC] font-primary text-[#333333] sm:bg-gradient-to-r sm:from-[#fbc2eb] sm:to-[#a6c1ee]">
       <ContactUs />
       <Toaster richColors />
     </div>
